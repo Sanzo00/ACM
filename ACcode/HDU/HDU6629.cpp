52b5617bd0784a755915e5610752d9bab4355db9
henuyh <2102370669@qq.com>
2019-08-05 23:29:48 +0800

    update_2019/08/05

---
+++ ACcode/HDU/HDU6629.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+const int maxn = 1e6 + 5;
+using namespace std;
+struct exKmp{
+    // 字符串下标从0开始
+    int nex[maxn], ex[maxn]; //模式串nex，匹配串ex
+    void getNex(char *str, int len) {
+        int i = 0, j, pos;
+        nex[0] = len;
+        while (str[i] == str[i+1] && i+1 < len) ++i;
+        nex[1] = i;
+        pos = 1;
+        for (int i = 2; i < len; ++i) {
+            if (nex[i-pos] + i < nex[pos] + pos) nex[i] = nex[i-pos];
+            else {
+                j = nex[pos] + pos - i;
+                if (j < 0) j = 0;
+                while (i+j < len && str[j] == str[j+i]) ++j;
+                nex[i] = j;
+                pos = i;
+            }
+        }
+    }
+    void getEx(char *s1, char *s2) { // s1匹配s2
+        int i = 0, j, pos;
+        int len1 = strlen(s1);
+        int len2 = strlen(s2);
+        getNex(s2, len2);
+        while (s1[i] == s2[i] && i < len1 && i < len2) ++i;
+        ex[0] = i;
+        pos = 0;
+        for (int i = 1; i < len1; ++i) {
+            if (nex[i-pos] + i < ex[pos] + pos) ex[i] = nex[i-pos];
+            else {
+                j = ex[pos] + pos - i;
+                if (j < 0) j = 0;
+                while (i+j < len1 && j < len2 && s1[i+j] == s2[j]) ++j;
+                ex[i] = j;
+                pos = i;
+            }
+        }
+    }
+}exkmp;
+char s[maxn];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        scanf("%s", s);
+        exkmp.getEx(s, s);
+        int len = strlen(s);
+        long long ans = 0;
+        for (int i = 1; i < len; ++i) {
+            ans += exkmp.ex[i];
+            if (i + exkmp.ex[i] < len) ans++;
+        }
+        printf("%lld\n", ans);
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ BZOJ                          3
 Codeforces                    76
 CometOJ                       5
 ECNUOJ                        4
-HDU                           41
+HDU                           42
 HPUOJ                         11
 Jisuanke                      2
 Kattis                        2
@@ -20,4 +20,4 @@ ZOJ                           2
 hihoCoder                     10
 zzuliOJ                       8
 ```
-## Total: 245
+## Total: 246
