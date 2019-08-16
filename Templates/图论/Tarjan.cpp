314eb4d78efea2673a58a35e0180a86f67b93d87
henuyh <2102370669@qq.com>
2019-08-16 17:56:29 +0800

    update_2019/08/16

--- Templates/图论/Tarjan.cpp
+++ Templates/图论/Tarjan.cpp
@@ -31,3 +31,36 @@ for (int i = 1; i <= n; ++i) {
     if (dfn[i]) continue;
     tarjan(i);
 }
+
+// 双连通分量
+vector<int> g[maxn];
+int dfn[maxn], low[maxn], Stack[maxn], inStack[maxn];
+int len, cnt, ts;
+void init(int n) {
+    len = cnt = ts = 0;
+    for (int i = 1; i <= n; ++i) g[i].clear();
+    fill(dfn, dfn+n+1, 0);
+}
+void tarjan(int u, int fa) {
+    dfn[u] = low[u] = ++ts;
+    Stack[len++] = u;
+    for (int i = 0; i < (int)g[u].size(); ++i) {
+        int v = g[u][i];
+        if (v == fa) continue;
+        if (!dfn[v]) {
+            // Stack[len++] = {u, v};
+            tarjan(v, u);
+            low[u] = min(low[u], low[v]);
+            if (dfn[u] <= low[v]) {
+                fill(inStack, inStack+n+1, 0);
+                inStack[u] = 1; 
+                while (1) {
+                    int top = Stack[--len];
+                    inStack[top] = 1; // 记录每次的连通分量中的点
+                    if (top == v) break; // top.u == u && top.v == top.v
+                }
+                // other check()
+            }
+        }else low[u] = min(low[u], dfn[v]);
+    }
+}
