308ec90670c7b3153682e302170109f0ac180c07
henuyh <2102370669@qq.com>
2019-08-27 10:03:32 +0800

    update_2019/08/27

---
+++ ACcode/HDU/HDU6687.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+const int maxn = 1e5 + 5;
+const int mod = 1e9 + 7;
+const int inf = 0x3f3f3f3f;
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    int a = 1 << 30; 
+    cout << a << endl;
+    return 0;
+}
